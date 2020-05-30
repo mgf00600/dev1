e87bf362e9b317c15f9b590b8cd61e78170bd46f
Mark G Freeman <markfreemanalbany@gmail.com>
2020-05-30 11:33:13 -0400

    zvca

--- FirstOnDevins/src/FirstOnDevins.java
+++ FirstOnDevins/src/FirstOnDevins.java
@@ -3,8 +3,9 @@ public class FirstOnDevins {
 	
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
-System.out.println("Hello World FirstOnDevins laptop X1");
+System.out.println("Hello World FirstOnDevins laptop X1x");
 	}
 
 }
